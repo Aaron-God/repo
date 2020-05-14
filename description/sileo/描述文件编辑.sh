312ccfd884c80642aec8f6d5b167504e400d90e0
yycaaron <shicuncunla@gmail.com>
2020-05-15 07:07:27 +0800

    更新

--- description/sileo/描述文件编辑.sh
+++ description/sileo/描述文件编辑.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/小火箭/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852359/Quantumult/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/小火箭砸壳版本/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/Quantumult砸壳版本/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/11.0-13.4.5/g"  cydia副本 sileo副本.json
 
